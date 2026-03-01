cfad2ef72c06f8ee5513113cdce54a15ecfafdcd
Michał Kowal <michal.kowal.66@gmail.com>
2026-03-01 18:44:27 +0100

    Add simple routing

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 
+require_once "Routing.php";
 
-echo "<h1>Hi there Adrian!</h1>";
+$path = trim($_SERVER["REQUEST_URI"], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::run($path);
