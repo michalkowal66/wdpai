66cf9afbe5934e1da987e4d2c31678b4a2edbd80
Michał Kowal <michal.kowal.66@gmail.com>
2026-03-29 09:04:12 +0200

    Add routing table, use controllers in routing class

--- Routing.php
+++ Routing.php
@@ -1,19 +1,48 @@
 <?php
 
+require_once 'src/controllers/SecurityController.php';
+require_once 'src/controllers/DashboardController.php';
+
+// TODO musimy zapewnic, ze utworzony 
+// obiekt kontrollera ma tylko jedna instancję - SINGLETON
+
+// TODO 2 /dashboard -- wszystkie dnae
+// /dashboard/12234 -- wyciagnie nam jakis element o wskaznaym ID 12234
+// REGEX
 class Routing {
-    public static function run($path) {
-        switch ($path) {
-            case "dashboard":
-                include "public/views/dashboard.html";
-                break;
-            case "login":
-                include "public/views/login.html";
-                break;
-            case "":
-                include "public/views/index.html";
-                break;
+
+    public static $routes = [
+        "login" => [
+            "controller" => "SecurityController",
+            "action" => "login"
+        ],
+        "dashboard" => [
+            "controller" => "DashboardController",
+            "action" => "index"
+        ],
+        "" => [
+            "controller" => "SecurityController",
+            "action" => "login"
+        ],
+    ];
+
+    public static function run(string $path) {
+        // TODO sprawdzać za pomoca array_key_exists
+
+        switch($path) {
+            case 'dashboard':
+            case '':
+            case 'login':
+                $controller = Routing::$routes[$path]["controller"];
+                $action = Routing::$routes[$path]["action"];
+
+                $controllerObj = new $controller;
+                $id = null;
+
+                $controllerObj->$action($id);
+                break; 
             default:
-                include "public/views/404.html";
+                include 'public/views/404.html';
                 break;
         }
     }
