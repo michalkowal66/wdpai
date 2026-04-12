4a220c5908f980f4946e47ea4e449bccd5569bad
Michał Kowal <michal.kowal.66@gmail.com>
2026-04-12 15:32:39 +0200

    Fetch data from db in dashboard controller, display on page

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -1,14 +1,15 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../repositories/UsersRepository.php';
 
 class DashboardController extends AppController {
 
     public function index() {
-        // TODO pobieranie danych z bazy
-        // wstawianie zmiennych na widok
         $title = "INDEX";
+        $usersRepository = new UsersRepository();
+        $users = $usersRepository->getUsers();
 
-        return $this->render("index", ["title" => $title]);
+        return $this->render("index", ["title" => $title, "users" => $users]);
     }
 }
