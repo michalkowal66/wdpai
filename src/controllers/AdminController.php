eaeaf790309ba88354120c3972fc1ace21e61c48
Michał Kowal <michal.kowal.66@gmail.com>
2026-05-02 15:06:56 +0200

    Add intial implementation of users fetching in users page

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -1,12 +1,18 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../repositories/UsersRepository.php';
 
 class AdminController extends AppController {
 
     public function users() {
         $title = "HotDesk - User Management";
+        $usersRepository = new UsersRepository();
+        $usersList = $usersRepository->getUsers();
 
-        return $this->render("users", ["title" => $title]);
+        return $this->render("users", [
+            "title" => $title,
+            "users" => $usersList
+        ]);
     }
 }
