92ceb6254c7b03b0c8b5eeeea62d10301ac053c2
Michał Kowal <michal.kowal.66@gmail.com>
2026-05-02 13:24:41 +0200

    Use env file to config app

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+// Database connection constants pulled from environment variables
+define('USERNAME', getenv('POSTGRES_USER') ?: 'postgres');
+define('PASSWORD', getenv('POSTGRES_PASSWORD') ?: 'postgres');
+define('HOST', getenv('POSTGRES_HOST') ?: 'db');
+define('DATABASE', getenv('POSTGRES_DB') ?: 'hotdesk');
